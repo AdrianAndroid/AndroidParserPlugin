554f19df7d78d2a0e3c312a16eb2a2b605128874
zhaojian <flannery.zhao@kikatech.com>
2022-10-08 10:20:59 +0800

    first commit

---
+++ android_project/libjavakotlin/src/main/java/com/example/libjavakotlin/MyClass.java
@@ -0,0 +1,9 @@
+package com.example.libjavakotlin;
+
+import javax.annotation.processing.Messager;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
